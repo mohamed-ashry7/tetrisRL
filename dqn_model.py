3b8155fe014014f36a308c2707822ae9e5192f85
mohamed-ashry <mohamed.mahmoud@uni-ulm.de>
2020-06-16 15:13:14 +0200

    updating the model

--- dqn_model.py
+++ dqn_model.py
@@ -10,42 +10,34 @@ class DQN(nn.Module):
     def __init__(self,input_shape,number_actions):
         super(DQN,self).__init__()
         input_shape=np.prod(input_shape)
-#         self.conv_layers=nn.Sequential(
-#             nn.Conv2d(1, 16, kernel_size=3, stride=1),
-#             nn.BatchNorm2d(16),
-#             nn.ReLU(),
-#             nn.Conv2d(16, 32, kernel_size=4, stride=2),
-#             nn.BatchNorm2d(32),
-#             nn.ReLU()
-#         )
         
-#         linear_input_shape=self._conv_to_linear_shape(input_shape)
-#         print(linear_input_shape)
         self.fc_layers=nn.Sequential(
             nn.Linear(input_shape, 256),
+            nn.BatchNorm1d(256),
             nn.ReLU(),
+            nn.Dropout(0.3),
             nn.Linear(256, 200),
+            nn.BatchNorm1d(200),
             nn.ReLU(),
             nn.Linear(200, 160),
+            nn.BatchNorm1d(160),
             nn.ReLU(),
+            nn.Dropout(0.3),
             nn.Linear(160, 110),
+            nn.BatchNorm1d(110),
             nn.ReLU(),
             nn.Linear(110, 70),
+            nn.BatchNorm1d(70),
             nn.ReLU(),
-            nn.Linear(70, number_actions),
-            nn.ReLU()
+            nn.Linear(70, number_actions)
             
         )
 
         
-#     def _conv_to_linear_shape(self,shape):
-#         o=self.conv_layers(torch.zeros(1,*shape))
-#         return int(np.prod(o.size()))
     
     def flat_features_number(self,x):
         features_size=x.size()[1:]
         return np.prod(features_size)
     
     def forward(self,x):
-#         x=self.conv_layers(x)
         return self.fc_layers(x.view(-1,self.flat_features_number(x)))
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
 NN+Dellacherie+Schwenker
+BatchNorm1D
+https://medium.com/syncedreview/batchnorm-dropout-dnn-success-eed740e1ca13
+https://arxiv.org/pdf/1502.03167.pdf
+BatchNorm+Dropout
+https://stackoverflow.com/questions/39691902/ordering-of-batch-normalization-and-dropout
+https://arxiv.org/pdf/1801.05134.pdf
+https://www.cs.toronto.edu/~hinton/absps/JMLRdropout.pdf
+Large Batch Size 
+https://openai.com/blog/science-of-ai/
