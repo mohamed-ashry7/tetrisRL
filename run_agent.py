63682b370fb91581857f5c517562a7e4aa733375
mohamedashry7 <mohamedelashry7777@gmail.com>
2020-12-29 20:29:13 +0200

    Make the code more generic

--- run_agent.py
+++ run_agent.py
@@ -9,6 +9,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import time
+import argparse
 
 
 GAMMA=0.99
@@ -69,10 +70,24 @@ if __name__=="__main__":
 
 
 
-  device=torch.device("cuda" if torch.cuda.is_available() else "cpu")
+  parser=argparse.ArgumentParser()
+  parser.add_argument("--cuda",default=False,help="Enable cuda",action="store_true")
+  parser.add_argument("--width",default=10,help="Board Width",action="store")
+  parser.add_argument("--height",default=20,help="Board Height",action="store")
+  parser.add_argument("--mode",default='train',help="Define the mode of the model play or train",action="store")
+  parser.add_argument("--model",default=None,help="Model file to load",action='store')
+  parser.add_argument("--model-dir",default="./dqn_models_stats/",help="Directory to save models at it",action='store')
+  parser.add_argument("--play-times",default=None,help="Testing the model how many times",action='store')
+  
+  args=parser.parse_args()
+    
+  device=torch.device("cuda" if args.cuda else "cpu")
+  env=TetrisEngine(args.width,args.height)  
+
+
   envs =[] 
   for _ in range(N_ENVS):
-    env=TetrisEngine(10,20)
+    env=TetrisEngine(args.width,args.height)  
     envs.append(env)
 
   net=DQN(env.state_shape(),env.number_actions()).to(device)
@@ -83,77 +98,80 @@ if __name__=="__main__":
   agent=Agent(envs,replay_buffer)
 
 
-  model_path="./drive/My Drive/Colab Notebooks/exp9/"
-  mode ='train'
-
+  model_path=args.model_dir
+  mode = args.mode
+  model = 
   if mode =='train':
-      state = torch.load(model_path+"tetris_best_124925.dat", map_location=lambda stg,_: stg)# FIRST ONE 348 + 10702 
-      net.load_state_dict(state)
-      epsilon = EPSILON_START
-      optimizer=torch.optim.Adam(net.parameters(),lr=LEARNING_RATE)
 
-      total_rewards, mean_rewards = [], []
-      q_losses, mean_q_losses=[], []
-      cleared_lines, mean_cleared_lines=[], []
-      tetrominos, mean_tetrominos =[], []
-      frame_idx, ts_frame = 0, 0
-      
-      ts = time.time()
+    if args.model != None:
+        state = torch.load(model_path+args.model, map_location=lambda stg,_: stg)# to train from a previous data.
+        net.load_state_dict(state)
+    epsilon = EPSILON_START
+    optimizer=torch.optim.Adam(net.parameters(),lr=LEARNING_RATE)
 
-      while True:
-          frame_idx += 1
-          for _ in range(N_ENVS):
-            epsilon = max(EPSILON_FINAL, EPSILON_START - frame_idx / EPSILON_DECAY_LAST_FRAME)
-            r,c,t = agent.play_step(net, epsilon, device=device)
+    total_rewards, mean_rewards = [], []
+    q_losses, mean_q_losses=[], []
+    cleared_lines, mean_cleared_lines=[], []
+    tetrominos, mean_tetrominos =[], []
+    frame_idx, ts_frame = 0, 0
+    
+    ts = time.time()
+
+    while True:
+        frame_idx += 1
+        for _ in range(N_ENVS):
+          epsilon = max(EPSILON_FINAL, EPSILON_START - frame_idx / EPSILON_DECAY_LAST_FRAME)
+          r,c,t = agent.play_step(net, epsilon, device=device)
+          
+          if r is not None:
+              cleared_lines.append(c)
+              mean_cleared_lines.append(np.mean(cleared_lines[-100:]))
+
+              tetrominos.append(t)
+              mean_tetrominos.append(np.mean(tetrominos[-100:]))
+
+              total_rewards.append(r)
+              m_reward = np.mean(total_rewards[-100:])
+              mean_rewards.append(m_reward)
+
+              speed = (frame_idx - ts_frame) / (time.time() - ts)
+              ts_frame = frame_idx
+              ts = time.time()
+              ql=0.0 if len(mean_q_losses)==0 else mean_q_losses[-1]
+              if len(total_rewards)%100==0:
+                print("%d: done %d games, reward %.3f, "
+                "eps %.2f, lines %.3f, Q-losses %.3f, speed %.2f f/s" % (frame_idx, len(total_rewards), m_reward, epsilon, mean_cleared_lines[-1],ql,speed))
             
-            if r is not None:
-                cleared_lines.append(c)
-                mean_cleared_lines.append(np.mean(cleared_lines[-100:]))
 
-                tetrominos.append(t)
-                mean_tetrominos.append(np.mean(tetrominos[-100:]))
+        if len(replay_buffer) < REPLAY_START_SIZE:
+            continue
+        if frame_idx % SYNC_TARGET_FRAMES == 0:
+            target_net.load_state_dict(net.state_dict())
+        
 
-                total_rewards.append(r)
-                m_reward = np.mean(total_rewards[-100:])
-                mean_rewards.append(m_reward)
-
-                speed = (frame_idx - ts_frame) / (time.time() - ts)
-                ts_frame = frame_idx
-                ts = time.time()
-                ql=0.0 if len(mean_q_losses)==0 else mean_q_losses[-1]
-                if len(total_rewards)%100==0:
-                  print("%d: done %d games, reward %.3f, "
-                  "eps %.2f, lines %.3f, Q-losses %.3f, speed %.2f f/s" % (frame_idx, len(total_rewards), m_reward, epsilon, mean_cleared_lines[-1],ql,speed))
-              
-
-          if len(replay_buffer) < REPLAY_START_SIZE:
-              continue
-          if frame_idx % SYNC_TARGET_FRAMES == 0:
-              target_net.load_state_dict(net.state_dict())
-          
-
-          batch =replay_buffer.sample(BATCH_SIZE)
-          loss=optimize_network(optimizer,batch ,net,target_net,device )
-          q_losses.append(loss)
-          mean_q_losses.append(np.mean(q_losses[-100:]))
-          
-         
-          if frame_idx %DRAWING_RATE ==0:
-              games_number=len(total_rewards)
-              plot_data(model_path+f"Rewards_of_{games_number}_games.png",mean_rewards,'Rewards',games_number)
-              plot_data(model_path+f"Losses_of_{games_number}_games.png",mean_q_losses,'Q losses',games_number)
-              plot_data(model_path+f"Cleared_lines_of_{games_number}_games.png",mean_cleared_lines,'Cleared Lines',games_number)
-              plot_data(model_path+f"Tetrominos_of_{games_number}_games.png",mean_tetrominos,'Tetrominos',games_number)
-              hist_data(model_path+f"actions_distributions{games_number}.png",agent.actions,"Actions")
-              torch.save(net.state_dict(), f"{model_path}tetris_best_%d.dat" % games_number)
-  else:
-      
-      state = torch.load(model_path+"tetris_best_124925.dat", map_location=lambda stg,_: stg)
-      net.load_state_dict(state)
-      for i in range (100):
+        batch =replay_buffer.sample(BATCH_SIZE)
+        loss=optimize_network(optimizer,batch ,net,target_net,device )
+        q_losses.append(loss)
+        mean_q_losses.append(np.mean(q_losses[-100:]))
+        
+        
+        if frame_idx %DRAWING_RATE ==0:
+            games_number=len(total_rewards)
+            plot_data(model_path+f"Rewards_of_{games_number}_games.png",mean_rewards,'Rewards',games_number)
+            plot_data(model_path+f"Losses_of_{games_number}_games.png",mean_q_losses,'Q losses',games_number)
+            plot_data(model_path+f"Cleared_lines_of_{games_number}_games.png",mean_cleared_lines,'Cleared Lines',games_number)
+            plot_data(model_path+f"Tetrominos_of_{games_number}_games.png",mean_tetrominos,'Tetrominos',games_number)
+            hist_data(model_path+f"actions_distributions{games_number}.png",agent.actions,"Actions")
+            torch.save(net.state_dict(), f"{model_path}tetris_best_%d.dat" % games_number)
+else:
+    if args.model==None:
+          parser.error("--mode play requires --model")
+    state = torch.load(model_path, map_location=lambda stg,_: stg)
+    net.load_state_dict(state)
+    for i in range (args.play_time):
+      r,c,t=agent.play_step(net,mode='play',device=device)
+      while r==None:
+        # print(envs[0])
         r,c,t=agent.play_step(net,mode='play',device=device)
-        while r==None:
-          # print(envs[0])
-          r,c,t=agent.play_step(net,mode='play',device=device)
-        print (f'Reward= {r}, Cleared_lines={c}, Tetrominos{t}')
-      hist_data(model_path+"actions_distributions.png",agent.actions,"Actions")  
+      print (f'Reward= {r}, Cleared_lines={c}, Tetrominos{t}')
+    hist_data(model_path+"actions_distributions.png",agent.actions,"Actions")  
